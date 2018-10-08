33343deebc4a4af5175ca73db9ff532d911b0c07
Poffertjeskip <30628312+ticho10@users.noreply.github.com>
2018-10-08 21:20:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+/*Hier pak ik de id's */
+
+
+function english() {
+    document.getElementById('header').innerHTML =  "English";
+
+}
+function dutch() {
+    document.getElementById('header').innerHTML =  "Dutch";
+
+}
