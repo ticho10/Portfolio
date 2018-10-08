e3e7b604d2a94fef0502868a15fd802a0747770a
Poffertjeskip <30628312+ticho10@users.noreply.github.com>
2018-10-08 23:02:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,9 +4,14 @@
 
 function english() {
     document.getElementById('header').innerHTML =  "English";
+    document.getElementById('portfolio').innerHTML =  "This is the portfolio of Ticho Plug";
+    document.getElementById('header').innerHTML =  "Dutch";
+    document.getElementById('header').innerHTML =  "Dutch";
 
 }
 function dutch() {
     document.getElementById('header').innerHTML =  "Dutch";
-
+    document.getElementById('portfolio').innerHTML =  "Dit is de portfolio van Ticho Plug";
+    document.getElementById('header').innerHTML =  "Dutch";
+    document.getElementById('header').innerHTML =  "Dutch";
 }
